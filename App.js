fccbc145ed216d2b62a6798d995cb742c312fa9d
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2022-05-31 13:11:13 -0500

    Lista de users

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Peliculas from './Peliculas'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <Peliculas/>
+      {/* <View style={styles.container}>
+        <Card>
+        <Image
+              style={{width:"100%",height:100}}
+              resizeMode="contain"
+              source={{ uri: "https://avatars0.githubusercontent.com/u/32242596?s=460&u=1ea285743fc4b083f95d6ee0be2e7bb8dcfc676e&v=4" }}
+        />
+        <Card.Title>Marco Antonio Carrillo Osuna</Card.Title>
+        <Card.Divider/>
+        <Text>Edad: 20</Text>
+         </Card>
+      </View> */}
+      
+    </SafeAreaProvider>
   );
 }
 
